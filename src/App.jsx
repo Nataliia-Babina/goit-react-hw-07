4792bc626fbecb2b9be28762084d8098ee588220
Nataliia Babina <157317342+Nataliia-Babina@users.noreply.github.com>
2024-09-17 18:03:56 +0200

    Change App

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,24 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import ContactForm from "./components/ContactForm/ContactForm";
 import ContactList from "./components/ContactList/ContactList";
 import SearchBox from "./components/SearchBox/SearchBox";
+import { fetchContacts } from "./redux/contactsOps";
+import { useDispatch } from "react-redux";
 
-const initialContacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
-
-export const App = () => {
-  const [contacts, setContacts] = useState(() => {
-    const storedContacts = JSON.parse(localStorage.getItem('storedContacts'));
-    return storedContacts || initialContacts;
-  });
+const App = () => {
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    localStorage.setItem('storedContacts', JSON.stringify(contacts));
-  }, [contacts]);
+  dispatch(fetchContacts());
+  }, [dispatch]);
 
-  const [nameFilter, setNameFilter] = useState('');
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts) => {
-      return [...prevContacts, newContact];
-    });
-  };
-
-  const deleteContact = (contactId) => {
-    setContacts((prvContacts) => {
-      return prvContacts.filter((contact) => contact.id !== contactId);
-    });
-  };
-
-  const visibleContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(nameFilter.toLowerCase())
-  );
 
   return (
     <>
       <h1>PhoneBook</h1>
-      <ContactForm onAdd={addContact} />
-      <SearchBox value={nameFilter} onChange={setNameFilter} />
-      <ContactList items={visibleContacts} onDelete={deleteContact} />
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
     </>
   );
 };
